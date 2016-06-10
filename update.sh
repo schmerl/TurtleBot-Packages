8ece9fc10b9fe9ae8e55b0bc265d07cef826f83b
Anurag Kanungo <anuragkanungo@gmail.com>
2016-06-10 12:18:39 -0700

    Install local packages using dpkg instead of apt

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
  #! /bin/bash
- rm -rf /usr/local/turtlebot-debs
- mkdir -p /usr/local/turtlebot-debs
- cp -a debs/* /usr/local/turtlebot-debs/
- cd /usr/local/turtlebot-debs
+ cd ./debs
  dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
- sed -i -e '1ideb file:/usr/local/turtlebot-debs ./\' /etc/apt/sources.list 
+ dpkg -i *.deb
